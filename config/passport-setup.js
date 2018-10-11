d72eded60f931a68bee4c1701d097d3183634419
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-10-11 22:44:20 +0530

    Serilaized user

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+})
+
 passport.use(
     new GoogleStrategy({
         // options for google strategy
@@ -12,12 +22,25 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
         console.log('passport callback function fired:');
-        console.log(profile);
-        new User({
-            googleId: profile.id,
-            username: profile.displayName
-        }).save().then((newUser) => {
-            console.log('new user created: ', newUser);
-        });
+        // check if user exist already in our db
+
+        User.findOne({googleId: profile.id}).then( (currentUser) => {
+            if(currentUser){
+                // allready have the user
+                console.log('user is:', currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('new user created: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        })
+
+        
     })
 );
